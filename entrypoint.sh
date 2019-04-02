6e3236dc28601cfa8e241540bc3a252f9b8cc9d5
Terwer <youweics@sina.com>
2019-04-02 15:02:48 +0800

    Add code volumn

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,25 @@ cp /sources.list /etc/apt/sources.list
 # cat /etc/apt/sources.list
 apt-get update
 
-# 安装git
-apt-get install git -y
-git --version
-
 # 如果不存在源码，重新拉取源码
-# git clone https://github.com/terwer/dragonwell8.git
+if [[ -d dragonwell8 ]]; then
+  error "directory dragonwell8 already exist"
+else
+  # 安装git
+  apt-get install git -y
+  git --version
+  git clone https://github.com/terwer/dragonwell8.git
+  # 获取子模块
+  chmod +x ./get_source_dragonwell.sh
+  bash ./get_source_dragonwell.sh -s github
+fi
+
+tree
+cat ./a.txt
+
+# 编译配置
+# chmod +x ./configure
+
+# 编译源码
 
 /bin/bash
